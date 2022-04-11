6e75ec43ff3f74c445db7b66d285b08bc6911158
Henrik Knutsen <henrik.knutsen@kleppnett.no>
2022-04-11 13:54:32 +0200

    Add test directory

---
+++ test/fixture/main.tf
@@ -0,0 +1,17 @@
+provider "azurerm" {
+  features {}
+}
+
+locals {
+  application = random_id.this.hex
+  environment = "test"
+}
+
+resource "random_id" "this" {
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "rg-${local.application}-${local.environment}"
+  location = var.location
+}
