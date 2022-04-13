dceb52daee6aa0d95469e62c6e186ac5cd22c2e2
Henrik Knutsen <henrik.knutsen@kleppnett.no>
2022-04-13 14:10:03 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
 locals {
+  suffix       = "${var.application}-${var.environment}"
+  suffix_alnum = join("", regexall("[a-z0-9]", lower(local.suffix)))
+  
   tags = merge({ application = var.application, environment = var.environment }, var.tags)
 }
