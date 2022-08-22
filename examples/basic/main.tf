201667bfa602272d502a554811b2c27a5ac69a90
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2022-08-22 13:43:25 +0200

    Update test and examples

---
+++ examples/basic/main.tf
@@ -0,0 +1,20 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "random_id" "this" {
+  byte_length = 8
+}
+
+resource "azurerm_resource_group" "this" {
+  name     = "rg-${random_id.this.hex}"
+  location = var.location
+}
+
+module "foobar" {
+  # source = "github.com/equinor/terraform-azurerm-foobar?ref=v1.0.0"
+  source = "../.."
+
+  resource_group_name = azurerm_resource_group.this.name
+  location            = azurerm_resource_group.this.location
+}
