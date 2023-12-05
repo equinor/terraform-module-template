b8eb31e82513a35a498486e9be2e8c45a6d38daa
Henrik Knutsen <46495473+hknutsen@users.noreply.github.com>
2023-12-05 13:43:51 +0100

    docs: development and testing

--- examples/basic/variables.tf
+++ examples/basic/variables.tf
@@ -1,5 +1,9 @@
+variable "resource_group_name" {
+  description = "The name of the resource group to create the resources in."
+  type        = string
+}
+
 variable "location" {
   description = "The location to create the resources in."
   type        = string
-  default     = "northeurope"
 }
